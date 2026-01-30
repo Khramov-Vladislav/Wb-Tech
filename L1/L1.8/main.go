6743810f60e501e191b596a1065d403aa5491666
Vladislav Khramov <209630394+Khramov-Vladislav@users.noreply.github.com>
2026-01-31 00:27:44 +0300

    Выполнено L1.8

---
+++ L1/L1.8/main.go
@@ -0,0 +1,24 @@
+// Дана переменная типа int64. Разработать программу, которая устанавливает i-й бит этого числа в 1 или 0.
+// Пример: для числа 5 (0101₂) установка 1-го бита в 0 даст 4 (0100₂).
+// Подсказка: используйте битовые операции (|, &^).
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var number int64 = 5
+	fmt.Printf("number: %d = %b\n", number, number)
+	var index int = 0
+
+	var mask int64 = 1 << index
+	fmt.Printf("mask: %d = %b\n", mask, mask)
+
+	if mask&number != 0 {
+		fmt.Printf("%d = %b\n", number&^mask, number&^mask)
+	} else {
+		fmt.Printf("%d = %b\n", number|mask, number|mask)
+	}
+}
