394be33d1813ab5964a9bc8a6bd59a6a80042a89
Vladislav Khramov <209630394+Khramov-Vladislav@users.noreply.github.com>
2026-01-31 21:57:35 +0300

    Выполнено L1.12

---
+++ L1/L1.12/main.go
@@ -0,0 +1,30 @@
+// Имеется последовательность строк: ("cat", "cat", "dog", "cat", "tree").
+// Создать для неё собственное множество.
+// Ожидается: получить набор уникальных слов. Для примера, множество = {"cat", "dog", "tree"}.
+
+package main
+
+import "fmt"
+
+
+func main() {
+	array1 := []string {"cat", "cat", "dog", "cat", "tree"}	
+	array2 := []string {}
+
+	for i := range array1 {
+		flag := false
+		for j := range array2 {
+			if array1[i] == array2[j] {
+				flag = true
+				break
+			}
+		}
+
+		if !flag {
+			array2 = append(array2, array1[i])
+		}
+	}
+
+	fmt.Println(array1)
+	fmt.Println(array2)
+}
