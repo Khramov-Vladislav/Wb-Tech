0edaa4c821305a4a9f7f6e31917371d758b5d661
Vladislav Khramov <209630394+Khramov-Vladislav@users.noreply.github.com>
2026-02-05 20:51:58 +0300

    Выполнено L1.15

---
+++ L1/L1.15/main.go
@@ -0,0 +1,38 @@
+/*
+	Рассмотреть следующий код и ответить на вопросы:
+	к каким негативным последствиям он может привести и как это исправить?
+	Приведите корректный пример реализации.
+	Вопрос: что происходит с переменной justString?
+*/
+
+/*
+var justString string
+
+func someFunc() {
+  v := createHugeString(1 << 10)
+  justString = v[:100]
+}
+
+func main() {
+  someFunc()
+}
+*/
+
+/*
+	проблема в том, что нам нужна строка в 100 символов, а будет храниться вся исходная строка на 1024 сивола,
+	что не эфективно
+*/
+
+package main
+
+var justString string
+
+func someFunc() {
+	v := createHugeString(1 << 10)
+	newString := v[:100]
+	justString = string(newString)
+}
+
+func main() {
+	someFunc()
+}
