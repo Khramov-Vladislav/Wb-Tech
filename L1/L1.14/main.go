b9eaedb22d04abdb08e7d24e0a987b477a0df6f0
Vladislav Khramov <209630394+Khramov-Vladislav@users.noreply.github.com>
2026-02-05 20:28:54 +0300

    сделано L1.14

---
+++ L1/L1.14/main.go
@@ -0,0 +1,43 @@
+// Разработать программу, которая в runtime способна определить тип переменной,
+// переданной в неё (на вход подаётся interface{}).
+// Типы, которые нужно распознавать: int, string, bool, chan (канал).
+// Подсказка: оператор типа switch v.(type) поможет в решении.
+
+package main
+
+import "fmt"
+
+func checkType(v interface{}) {
+	switch v.(type) {
+	case int:
+		fmt.Printf("тип %T\n", v)
+	case string:
+		fmt.Printf("тип %T\n", v)
+	case bool:
+		fmt.Printf("тип %T\n", v)
+	case chan int:
+		fmt.Printf("тип %T\n", v)
+	case chan string:
+		fmt.Printf("тип %T\n", v)
+	case chan bool:
+		fmt.Printf("тип %T\n", v)
+	default:
+		fmt.Println("Неизвестный тип данных")
+	}
+}
+
+func main() {
+	var typeInt int = 10
+	var typeString string = "Hello"
+	var typeBool bool = true
+	var typeChanInt chan int
+	var typeChanString chan string
+	var typeChanBool chan bool
+
+	checkType(typeInt)
+	checkType(typeString)
+	checkType(typeBool)
+	checkType(typeChanInt)
+	checkType(typeChanString)
+	checkType(typeChanBool)
+}
