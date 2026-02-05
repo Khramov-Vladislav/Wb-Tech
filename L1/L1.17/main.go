adf29a3cadd9fa37dc4e7468d699c48d3e0ba958
Vladislav Khramov <209630394+Khramov-Vladislav@users.noreply.github.com>
2026-02-05 22:06:26 +0300

    Выполнено L1.17

---
+++ L1/L1.17/main.go
@@ -0,0 +1,37 @@
+/*
+	Реализовать алгоритм бинарного поиска встроенными методами языка.
+	Функция должна принимать отсортированный слайс и искомый элемент,
+	возвращать индекс элемента или -1, если элемент не найден.
+
+	Подсказка: можно реализовать рекурсивно или итеративно, используя цикл for.
+*/
+
+package main
+
+import "fmt"
+
+func binarySearch(array []int, findNumber int) int {
+	left := 0
+	right := len(array) - 1
+
+	for right-left > 1 {
+		middle := (left + right) / 2
+		if array[middle] >= findNumber {
+			right = middle
+		} else {
+			left = middle
+		}
+	}
+
+	if array[right] == findNumber {
+		return right
+	} else {
+		return -1
+	}
+}
+
+func main() {
+	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	findNumber := 8
+	fmt.Println(binarySearch(array, findNumber))
+}
