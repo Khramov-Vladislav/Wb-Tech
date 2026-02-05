ba02a11a262e4a46b49e1df35baf2ef09db48819
Vladislav Khramov <209630394+Khramov-Vladislav@users.noreply.github.com>
2026-02-05 21:43:59 +0300

    Выполнено L1.16

---
+++ L1/L1.16/main.go
@@ -0,0 +1,49 @@
+/*
+	Реализовать алгоритм быстрой сортировки массива встроенными средствами языка.
+	Можно использовать рекурсию.
+
+	Подсказка: напишите функцию quickSort([]int) []int которая сортирует срез целых чисел.
+	Для выбора опорного элемента можно взять середину или первый элемент.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func quickSort(array []int) []int {
+	if len(array) <= 1 {
+		return array
+	}
+
+	mainElement := array[len(array)/2]
+	var leftPart []int
+	var rightPart []int
+	var medium []int
+
+	for i := range array {
+		if array[i] < mainElement {
+			leftPart = append(leftPart, array[i])
+		} else if array[i] == mainElement {
+			medium = append(medium, array[i])
+		} else if array[i] > mainElement {
+			rightPart = append(rightPart, array[i])
+		}
+	}
+
+	sortedLeftPart := quickSort(leftPart)
+	sortedRightPart := quickSort(rightPart)
+
+	var result []int
+	result = append(result, sortedLeftPart...)
+	result = append(result, medium...)
+	result = append(result, sortedRightPart...)
+
+	return result
+}
+
+func main() {
+	array := []int{1, 2, 10, 6, 3, 8, 0, 4, 2, 7}
+	fmt.Println(quickSort(array))
+}
