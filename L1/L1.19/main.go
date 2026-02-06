141e410bb3f8ada21737ad1584735aa70bf730ba
Vladislav Khramov <209630394+Khramov-Vladislav@users.noreply.github.com>
2026-02-06 18:23:35 +0300

    Выполнено L1.19

---
+++ L1/L1.19/main.go
@@ -0,0 +1,25 @@
+/*
+Разработать программу, которая переворачивает подаваемую на вход строку.
+Например: при вводе строки «главрыба» вывод должен быть «абырвалг».
+Учтите, что символы могут быть в Unicode (русские буквы, emoji и пр.),
+то есть просто iterating по байтам может не подойти — нужен срез рун ([]rune).
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func inversStr(str string) string {
+	sliceRune := []rune(str)
+	for i := 0; i < len(sliceRune)/2; i++ {
+		sliceRune[i], sliceRune[len(sliceRune)-1-i] = sliceRune[len(sliceRune)-1-i], sliceRune[i]
+	}
+	return string(sliceRune)
+}
+
+func main() {
+	str := "главрыба"
+	fmt.Println(inversStr(str))
+}
