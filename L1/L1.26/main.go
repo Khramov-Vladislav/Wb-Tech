8ba127099eb01934b54e588df874d565bd523811
Vladislav Khramov <209630394+Khramov-Vladislav@users.noreply.github.com>
2026-02-07 20:01:19 +0300

    Выполнено L1.26

---
+++ L1/L1.26/main.go
@@ -0,0 +1,43 @@
+/*
+Уникальные символы в строке
+
+Разработать программу, которая проверяет,
+что все символы в строке встречаются один раз (т.е. строка состоит из уникальных символов).
+
+Вывод: true, если все символы уникальны, false, если есть повторения.
+Проверка должна быть регистронезависимой, т.е. символы в разных регистрах считать одинаковыми.
+
+Например: "abcd" -> true, "abCdefAaf" -> false (повторяются a/A), "aabcd" -> false.
+
+Подумайте, какой структурой данных удобно воспользоваться для проверки условия.
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func findElem(str string) bool {
+	sliceRune := []rune((strings.ToLower(str)))
+	resultMap := make(map[rune]bool)
+	for _, v := range sliceRune {
+		if resultMap[v] {
+			return false
+		}
+		resultMap[v] = true
+	}
+
+	return true
+}
+
+func main() {
+	str1 := "abcd"
+	str2 := "abCdefAa"
+	str3 := "aabcd"
+
+	fmt.Println(findElem(str1))
+	fmt.Println(findElem(str2))
+	fmt.Println(findElem(str3))
+}
