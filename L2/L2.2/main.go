1b0da35e48eae3e5ff4cb2bdd1ea269ee13aa1a1
Vladislav Khramov <209630394+Khramov-Vladislav@users.noreply.github.com>
2026-02-09 17:48:34 +0300

    Выполнено L2.2

---
+++ L2/L2.2/main.go
@@ -0,0 +1,67 @@
+/*
+	Что выведет программа?
+
+	Объяснить порядок выполнения defer функций и итоговый вывод.
+*/
+
+/*
+	package main
+
+	import "fmt"
+
+	func test() (x int) {
+	defer func() {
+		x++
+	}()
+	x = 1
+	return
+	}
+
+	func anotherTest() int {
+	var x int
+	defer func() {
+		x++
+	}()
+	x = 1
+	return x
+	}
+
+	func main() {
+	fmt.Println(test())
+	fmt.Println(anotherTest())
+	}
+*/
+/*
+	Порядок вызова defer функций
+	defer функций вызывается в обратном порядке относительно вызова. В каждой функции сначало выполнятся defer, а уже потом управление будет передано main.
+
+	Что выведет программа?
+	В функции test() будет вывод 2, т.к возращается именнованная переменная, return ждет выполнения defer, который увеличивает x на 1
+	В функции anotherTest() будет вывод 1, т.к return захватывает переменную x и уже после этого идет выполнение defer, которые уже не влияют на значение переменной.
+*/
+
+package main
+
+import "fmt"
+
+func test() (x int) {
+	defer func() {
+		x++
+	}()
+	x = 1
+	return
+}
+
+func anotherTest() int {
+	var x int
+	defer func() {
+		x++
+	}()
+	x = 1
+	return x
+}
+
+func main() {
+	fmt.Println(test()) // 2
+	fmt.Println(anotherTest()) // 1
+}
