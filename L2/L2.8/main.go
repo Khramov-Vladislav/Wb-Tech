c51fc8d3487a7adcc212601136dda6a2c23c6ffa
Vladislav Khramov <209630394+Khramov-Vladislav@users.noreply.github.com>
2026-02-21 15:30:49 +0300

    Выполнено L2.8

--- L2/L2.8/main.go
+++ L2/L2.8/main.go
@@ -15,3 +15,27 @@
 */
 
 package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+func getNTP() (time.Time, error) {
+	ntpServer := "pool.ntp.org"
+	timeNTP, err := ntp.Time(ntpServer)
+	return timeNTP, err
+}
+
+func main() {
+	timeNTP, err := getNTP()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Ошибка получения времени NTP", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Точное время (NTP):", timeNTP)
+}
